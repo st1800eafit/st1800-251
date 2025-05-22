bcb10768343fd2a21499db7c6739369c4d205f42
edwinm67 <emontoya@eafit.edu.co>
2025-05-22 15:08:13 -0500

    spark

--- hadoop_spark/spark/wc-pyspark.py
+++ hadoop_spark/spark/wc-pyspark.py
@@ -4,5 +4,5 @@ spark = SparkSession.builder.appName("WordCount").getOrCreate()
 sc = spark.sparkContext
 
 files = sc.textFile("s3://emontoyadatalake/datasets/gutenberg-txt-es/*.txt")
-wc = files.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+wc = files.flatMap(lambda line: line.split()).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
 wc.coalesce(1).saveAsTextFile("hdfs:///tmp/wcout1")
